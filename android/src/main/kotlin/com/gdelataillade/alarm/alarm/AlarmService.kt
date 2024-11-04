b888e5d9eee6571b2b31f00e0a8f6d5637346798
arsallion313786 <99034072+arsallion313786@users.noreply.github.com>
2024-11-04 15:51:38 +0500

    Update AlarmService.kt

--- android/src/main/kotlin/com/gdelataillade/alarm/alarm/AlarmService.kt
+++ android/src/main/kotlin/com/gdelataillade/alarm/alarm/AlarmService.kt
@@ -214,7 +214,7 @@ class AlarmService : Service() {
         ))
 
         val am = getSystemService(AUDIO_SERVICE) as AudioManager
-        if (am.ringerMode != AudioManager.RINGER_MODE_SILENT && am.mode != AudioManager.MODE_IN_CALL) {
+        if (am.ringerMode != AudioManager.RINGER_MODE_SILENT && am.ringerMode != AudioManager.RINGER_MODE_VIBRATE && am.mode != AudioManager.MODE_IN_CALL) {
             if (volume >= 0.0 && volume <= 1.0) {
             volumeService?.setVolume(volume, showSystemUI)
         }
